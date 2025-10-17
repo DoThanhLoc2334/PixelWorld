ed80f63f28f3d90f7337d7c52bee69c868f553d8
Minh <duonganhminh05@gmail.com>
2025-10-17 15:02:55 +0700

    Update CanvasView.tsx

--- src/components/CanvasView.tsx
+++ src/components/CanvasView.tsx
@@ -2,24 +2,24 @@
 
 import { useEffect, useRef } from "react";
 import { createPixiApp } from "../pixi";
-import { Application, Container, Graphics} from "pixi.js" 
+import { Application, Container, Graphics } from "pixi.js"
 
 export default function CanvasView() {
     const canvasRef = useRef<HTMLDivElement>(null);
     let stage = new Container();
-    let cell = new Graphics().rect(500,50, 50, 50).fill('red');
+    let cell = new Graphics().rect(500, 50, 50, 50).fill('red');
     useEffect(() => {
         if (!canvasRef.current) return;
-        
-        let app : Application;
-        
+
+        let app: Application;
+
         (async () => {
             app = await createPixiApp(canvasRef.current!);
             stage.addChild(cell);
             app.stage.addChild(stage);
         })();
 
-        
+
 
         return () => {
             if (app) app.destroy(true, { children: true });
