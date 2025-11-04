9ab7ff871a7e3a830991174e296f5feb5a54d348
DoThanhLoc2334 <locdothanh233@gmail.com>
2025-11-04 09:02:56 +0700

    feat(server-side): create server.js

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+
+
+const app = express();
+app.use(cors());
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "*", // allow all origin
+    },
+});
+
+app.get("/", (req, res) => {
+    res.send("PixelWorld server is running!")
+});
+
+io.on("connection", (socket) =>{
+    console.log(" A user connected: ", socket.id);
+    socket.on("disconnect", () =>{
+        console.log("User disconnected:", socket.id);
+    });
+});
+
+const port = 3000;
+server.listen(port,() => {
+    console.log(`Server running on port ${port}`);
+}); 
