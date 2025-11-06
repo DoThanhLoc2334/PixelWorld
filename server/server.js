d44fe1c2efbcb2bebe85795962e87064436b7ac2
DoThanhLoc2334 <locdothanh233@gmail.com>
2025-11-06 09:26:41 +0700

    feat(server): handle cellClick event and send respone message to client.

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ import http from "http";
 import { Server } from "socket.io";
 import cors from "cors";
 
-
 const app = express();
 app.use(cors());
 const server = http.createServer(app);
@@ -17,16 +16,25 @@ app.get("/", (req, res) => {
     res.send("PixelWorld server is running!")
 });
 
-io.on("connection", (socket) =>{
+io.on("connection", (socket) => {
     console.log(" A user connected: ", socket.id);
-    socket.on("disconnect", () =>{
+
+    socket.on("cellClick", (data) => {
+        console.log(`User ${socket.id} clicked cell:`, data);
+    });
+
+    socket.emit("serverMessage", {
+        message: `Server nhận được click tại ô (${data.x}, ${data.y})`
+    });
+
+    socket.on("disconnect", () => {
         console.log("User disconnected:", socket.id);
     });
 });
 
 const port = 3000;
-server.listen(port,() => {
+server.listen(port, () => {
     console.log(`Server running on port ${port}`);
-}); 
+});
 
 
